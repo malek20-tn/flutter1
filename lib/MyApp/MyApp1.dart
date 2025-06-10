4b5b0727dac0b2db612bf1b22958f8d2e781acfb
Malekcherif <malekcherif251@gmail.com>
2025-06-10 12:37:46 +0100

    Update MyApp1.dart

--- lib/MyApp/MyApp1.dart
+++ lib/MyApp/MyApp1.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'SplashScreen.dart';
-class MyApp1 extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
