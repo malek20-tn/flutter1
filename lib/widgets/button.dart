7f361872f1ee75d7ff5677a6e473a6b3dd4d2898
Malekcherif <malekcherif251@gmail.com>
2025-07-06 23:18:00 +0100

    Update button.dart

--- lib/widgets/button.dart
+++ lib/widgets/button.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
 import '../screens/login.dart';
 import '../theme/color.dart';
-class Button  extends StatelessWidget {
+
+class Button extends StatelessWidget {
   final String text;
   final VoidCallback onPressed;
 
-
-  const Button ({super.key,required this.text,
-    required this.onPressed,});
+  const Button({
+    super.key,
+    required this.text,
+    required this.onPressed,
+  });
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: onPressed,
-      style: ElevatedButton.styleFrom(
-        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-        backgroundColor: Colors.blue,
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(Colors.blue),
       ),
       child: Text(
         text,
